b306242f06a66912bb9f9f5248b2bd26cc3252a1
omobomi oluwatosin <oluwatosinomobomi@gmail.com>
2019-12-06 11:01:09 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faChartBar,   faSearch, faUserFriends, faComment} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container"> 
+
+            <div className="discount">
+           <div className='list1'><h3> <FontAwesomeIcon icon={faSearch}  size={'lg'}/>  Follow your interests.</h3></div>
+           <div className='list2'><h3> <FontAwesomeIcon icon={faUserFriends}  size={'lg'}/> Hear what people are talking about.</h3></div>  
+           <div className='list3'><h3> <FontAwesomeIcon icon={faComment}  size={'lg'}/>Join the conversation.</h3></div>
+            </div>
+            <div className="network">
+               
+
+    
+            </div>
+            </div>
+
   );
 }
 
