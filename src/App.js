a7f09a237fcf64ae8363f1ded0ef1bcae5741838
omobomi oluwatosin <oluwatosinomobomi@gmail.com>
2019-12-07 22:30:25 -0800

    done

--- src/App.js
+++ src/App.js
@@ -10,14 +10,42 @@ function App() {
             <div className="discount">
            <div className='list1'><h3> <FontAwesomeIcon icon={faSearch}  size={'lg'}/>  Follow your interests.</h3></div>
            <div className='list2'><h3> <FontAwesomeIcon icon={faUserFriends}  size={'lg'}/> Hear what people are talking about.</h3></div>  
-           <div className='list3'><h3> <FontAwesomeIcon icon={faComment}  size={'lg'}/>Join the conversation.</h3></div>
+           <div className='list3'><h3> <FontAwesomeIcon icon={faComment}  size={'lg'}/>   Join the conversation.</h3></div>
             </div>
             <div className="network">
-               
+            <div className='form'>
+              
+<input type="text"  placeholder="Phone, email, or username" className='input'></input>
+
+<input type="text"  placeholder="Password" className='input1'></input>
+
+<button className='log'> Log in </button>
+
+<a class="forgot" href="/account/begin_password_reset" rel="noopener">Forgot password?</a>
+
+<div className='logo'>
+</div>
+
+<div className='go'><h2>See what’s happening in <p>the world right now</p></h2>
+
+<h3 className='join'>Join Twitter today.</h3>
+<div className='mon'>
+<button className='sign'>Sign up</button>
+</div>
+<button className='in'>Log in</button>
+</div>
+
+
+
+</div>
+
 
     
+ 
+                
             </div>
             </div>
+
 
   );
 }
